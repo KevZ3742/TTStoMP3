e00966396e4b803884c11772649dcc65c28157d6
KevZ3742 <66284340+KevZ3742@users.noreply.github.com>
2023-05-20 23:10:29 -0500

    Add files via upload

---
+++ TTS.py
@@ -0,0 +1,46 @@
+import pyttsx3
+import os
+
+engine = pyttsx3.init()
+
+num = 1
+name = "tts"
+string = "hello world"
+
+engine.save_to_file(string, "(" + str(num) + ") " + name + ".mp3")
+engine.runAndWait()
+
+source = "(" + str(num) + ") " + name + ".mp3"
+destination = "C:\\Users\\Kevin Zhang\\Videos\\TTS\\"
+
+def moveFile():
+    try:
+        if(os.path.exists(destination + source)):
+            action = input("File already exists (Override: o, Keep Both: k, Quit: q): ")
+
+            if(action == "o"):
+                os.replace(source, destination + source)
+                print(source + " was moved")
+            elif(action == "k"):
+                i = 1
+                while(checkExists(i)):
+                    i += 1
+                num = i
+                os.replace(source, destination + "(" + str(num) + ") " + name + ".mp3")
+                print(source + " was moved")
+            elif(action == "q"):
+                exit()
+        else:
+            os.replace(source, destination + source)
+            print(source + " was moved")
+    except FileNotFoundError:
+        print(source + " was not found")
+
+def checkExists(index):
+    path = destination + "(" + str(index) + ") " + name + ".mp3"
+    if(os.path.exists(path)):
+        return True
+    else:
+        return False
+
+moveFile()
